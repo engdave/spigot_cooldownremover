2ee047722f5f2c4161b6380770adbea69b9bc465
Dawid <121767492+engdave@users.noreply.github.com>
2023-02-14 16:24:16 +0100

    Add files via upload

--- src/main/java/me/engineerdave/cooldownremover/attackcooldownremover/AttackCooldownRemover.java
+++ src/main/java/me/engineerdave/cooldownremover/attackcooldownremover/AttackCooldownRemover.java
@@ -2,13 +2,12 @@ package me.engineerdave.cooldownremover.attackcooldownremover;
 
 import org.bukkit.Bukkit;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Objects;
 
 public final class AttackCooldownRemover extends JavaPlugin implements Listener {
 
@@ -25,13 +24,9 @@ public final class AttackCooldownRemover extends JavaPlugin implements Listener 
         }
 
         Player player = (Player) event.getDamager();
-        float attackSpeed = (float) Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_ATTACK_SPEED)).getValue();
-        float T = 0 / attackSpeed;
-        float t = (float) player.getLastDamage();
-        float damageMultiplier = 0.2f + (float) Math.pow((t + 0.5) / T, 2) * 0.8f;
-        damageMultiplier = Math.min(1, Math.max(0.2f, damageMultiplier));
-
-        Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_ATTACK_SPEED)).setBaseValue(20 / damageMultiplier - 0.5f);
+        AttributeInstance attackSpeed = player.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+        assert attackSpeed != null;
+        attackSpeed.setBaseValue(Double.POSITIVE_INFINITY);
     }
 
     @Override
